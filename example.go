58da09bdd44c229ca35cd3acf572b1d5152d58b6
MaYurD :) <md@pooraa.com>
2019-06-07 11:54:57 +0530

    code teak

--- example.go
+++ example.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/mayur-tolexo/sworker/draught"
 )
@@ -10,16 +12,18 @@ import (
 //print : function which worker will call to execute
 func print(ctx context.Context, value ...interface{}) error {
 	fmt.Println(value)
+	time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
 	return nil
 }
 
 //main function
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	handler := print                 //handler function which the go routine will call
-	n := 20                          //no of jobs
+	n := 10                          //no of jobs
 	pool := draught.NewSimplePool(n) //new job pool created
 	pool.SetTag("PRINTER")           //added tag to the pool
-	pool.SetProfiler(10)             //added profiler batch size
+	pool.SetProfiler(5)              //added profiler batch size
 	pool.SetConsoleLog(true)         //enable console log
 	pool.AddWorker(2, handler, true) //adding 2 workers
 
