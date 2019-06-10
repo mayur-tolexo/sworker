13dd7b4950fa921f36bad3b673a67e7174692f15
MaYurD :) <md@pooraa.com>
2019-06-11 00:49:54 +0530

    benchmarking added for other repos

--- benchmark_test.go
+++ benchmark_test.go
@@ -6,6 +6,10 @@ import (
 	"runtime"
 	"testing"
 
+	"github.com/Jeffail/tunny"
+	slaves "github.com/dgrr/GoSlaves"
+	"github.com/gammazero/workerpool"
+	"github.com/ivpusic/grpool"
 	"github.com/mayur-tolexo/sworker/draught"
 )
 
@@ -14,14 +18,15 @@ func print(ctx context.Context, value ...interface{}) (err error) {
 }
 
 func BenchmarkDraught(b *testing.B) {
-	for i := 1; i <= runtime.NumCPU()+1; i++ {
-		b.Run(fmt.Sprintf("%v-Worker", i), func(b *testing.B) {
-			runBenchmark(b, i)
-		})
-	}
+	// for i := 1; i <= runtime.NumCPU()+1; i++ {
+	n := runtime.NumCPU()
+	b.Run(fmt.Sprintf("%v-Worker", n), func(b *testing.B) {
+		runDraughtBenchmark(b, n)
+	})
+	// }
 }
 
-func runBenchmark(b *testing.B, wCount int) {
+func runDraughtBenchmark(b *testing.B, wCount int) {
 	handler := print
 	pool := draught.NewPool(b.N, "", nil)
 	pool.AddWorker(wCount, handler, true)
@@ -31,4 +36,88 @@ func runBenchmark(b *testing.B, wCount int) {
 		}
 	})
 	pool.Close()
+}
+
+func executeServe(p *slaves.Pool, rounds int) {
+	for i := 0; i < rounds; i++ {
+		p.Serve(i)
+	}
+}
+
+func BenchmarkSlavePool(b *testing.B) {
+	ch := make(chan int, b.N)
+
+	sp := slaves.NewPool(0, func(obj interface{}) {
+		ch <- obj.(int)
+	})
+
+	go executeServe(&sp, b.N)
+
+	i := 0
+	for i < b.N {
+		select {
+		case <-ch:
+			i++
+		}
+	}
+	close(ch)
+	sp.Close()
+}
+
+func BenchmarkGrPool(b *testing.B) {
+	n := runtime.NumCPU()
+	b.Run(fmt.Sprintf("%v-Worker", n), func(b *testing.B) {
+		// number of workers, and size of job queue
+		pool := grpool.NewPool(n, b.N)
+		defer pool.Release()
+
+		// how many jobs we should wait
+		pool.WaitCount(b.N)
+
+		// submit one or more jobs to pool
+		for i := 0; i < b.N; i++ {
+
+			pool.JobQueue <- func() {
+				// say that job is done, so we can know how many jobs are finished
+				defer pool.JobDone()
+			}
+		}
+
+		// wait until we call JobDone for all jobs
+		pool.WaitAll()
+	})
+}
+
+func BenchmarkTunny(b *testing.B) {
+	pool := tunny.NewFunc(10, func(in interface{}) interface{} {
+		intVal := in.(int)
+		return intVal * 2
+	})
+	defer pool.Close()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ret := pool.Process(10)
+		if exp, act := 20, ret.(int); exp != act {
+			b.Errorf("Wrong result: %v != %v", act, exp)
+		}
+	}
+}
+
+func BenchmarkWorkerpool(b *testing.B) {
+	n := runtime.NumCPU()
+	b.Run(fmt.Sprintf("%v-Worker", n), func(b *testing.B) {
+		wp := workerpool.New(n)
+		defer wp.Stop()
+		releaseChan := make(chan struct{})
+
+		b.ResetTimer()
+
+		// Start workers, and have them all wait on a channel before completing.
+		for i := 0; i < b.N; i++ {
+			wp.Submit(func() { <-releaseChan })
+		}
+		close(releaseChan)
+	})
 }
